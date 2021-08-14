c45699d66b49b75b963e8867a12d5d717d1b8a70
DoYouSellVHS <ghorta4@gmail.com>
2021-08-13 23:25:04 -0400

    Add project files.

---
+++ Guildleader/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Guildleader
+{
+    public class DebugClass
+    {
+        public void Greetings()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
---
+++ PMDMMO Main/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PMDMMO_Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
